4623513c38030ebf715cf2ae3e43cfccd30add39
Dedi Hartono <admin@dedihartono.com>
2023-10-25 19:45:17 +0700

    Fix Layouting - Home - App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Home, About, Contact, Login, Register, NotFound } from './components/pages';
 import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar/Navbar'
+import { Footer } from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Routes>
-        <Home />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
@@ -14,6 +16,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 };
